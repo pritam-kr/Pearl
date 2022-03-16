8087b9018abc6add4e00612b36541d65f389f81e
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-16 17:09:44 +0530

    Feat: Added Mockman

---
+++ src/component/Mockman/Mockman.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Mockman from "mockman-js";
+
+function MockAPI() {
+  return (
+      <>
+    <div className="MockAPI" style={{margin: "8rem 0 2rem 0"}}>
+      <Mockman />
+    </div>
+    </>
+  );
+}
+
+export default MockAPI;
