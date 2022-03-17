baaf6b83aea6a272320c751f186c32fcda005a11
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-17 23:58:27 +0530

    Some minor change in these page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import { BrowserRouter } from "react-router-dom"
-import { StateContextProvider } from  "./Context/GlobalContext/StateContext"
-import {AuthContextProvider} from "./Context/AuthContext/AuthContext"
+import { BrowserRouter } from "react-router-dom";
+import { StateContextProvider } from "./Context/GlobalContext/StateContext";
+import { AuthContextProvider } from "./Context/AuthContext/AuthContext";
 import { CartContextProvider } from "./Context/CartContext/CartContext";
- 
+
 
 // Call make Server
 makeServer();
@@ -15,16 +15,16 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-
       <StateContextProvider>
         <AuthContextProvider>
           <CartContextProvider>
-        <App />
-        </CartContextProvider>
+
+            <App />
+
+          </CartContextProvider>
         </AuthContextProvider>
       </StateContextProvider>
     </BrowserRouter>
-
   </React.StrictMode>,
   document.getElementById("root")
 );
