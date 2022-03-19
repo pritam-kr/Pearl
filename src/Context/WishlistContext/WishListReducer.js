abe4b943439727655f4d60db6e8eaed9788bb884
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-20 00:42:45 +0530

    Feat: Wishlist
    - Implemented Wishlist features
    - Created card for Wishlist card.
    - added remove feature from wishlist
    and also you can add these product to cart

---
+++ src/Context/WishlistContext/WishListReducer.js
@@ -0,0 +1,11 @@
+import {ADD_TO_WISHLIST} from "../Action/actions"
+export const wishListReducer = (state, action) => {
+    switch (action.type) {
+      case ADD_TO_WISHLIST:
+        return {
+          ...state,
+          wishlist: action.payload,
+          totalWishListItem: action.totalWishListItem,
+        };
+    }
+  };
---
+++ src/component/WishListProductCard/WishListCard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import * as BiIcons from "react-icons/bi";
+import { priceFormatter } from "../../utils/priceFormatter";
+import { useWishListContext } from "../../Context/index";
+import {useCartContext} from "../../Context/index"
+
+const WishListCard = ({ eachProduct }) => {
+    
+    const {addToCart} = useCartContext()
+
+    const { _id, image, title, price, rating, discount } = eachProduct;
+
+    const { removeFromWishlist } = useWishListContext();
+
+    return (
+        <div className="images-card badge-card product-card" key={_id}>
+            <img className="card-img" src={image} alt="product_image" />
+
+            <div className="card-content">
+                <h2 className="card-title product-title">{title}</h2>
+                <h1 className="card-price product-price">
+                    ₹ {priceFormatter(price)}
+                    <span className="discount-price"> {discount}</span>
+                </h1>
+                <p className="text-sm">
+                    <BiIcons.BiStar className="rating-start" /> {rating}/5
+                </p>
+            </div>
+
+            <div className="card-footer">
+                <div className="move-cart-buttons">
+                    <button className="btn btn-primary btn-move-cart text-sm center" onClick={() => addToCart(eachProduct)}>
+                        <BiIcons.BiCart className="cart-icon" /> Move to Cart
+                    </button>
+                </div>
+            </div>
+            <div class="btn-wishlist">
+                <BiIcons.BiTrash
+                    className="wishlist-icon"
+                    onClick={() => removeFromWishlist(eachProduct)}
+                />
+            </div>
+        </div>
+    );
+};
+
+export { WishListCard };
