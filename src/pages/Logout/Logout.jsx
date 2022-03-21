703b8f99ceb6f37b81e9c0af4df163db138e427f
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-21 17:15:25 +0530

    Created logout page

---
+++ src/pages/Logout/Logout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Logout.css"
+
+ const Logout = () => {
+     return(
+         <div className='logout' style={{margin: "8rem 0"}}>Logout</div>
+     )
+ }
+
+ export {Logout}
