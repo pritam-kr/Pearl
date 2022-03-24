5b8988edb3c08a34839314f9c9e4f62413382c9d
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-24 10:27:52 +0530

    Import toastify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import 'react-toastify/dist/ReactToastify.css';
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
