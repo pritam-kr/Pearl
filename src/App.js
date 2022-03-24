7574f85f4608313b2c26e730ae21211afa7f8f6c
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-24 09:59:30 +0530

    Import Toast container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
 import {Footer, Navigation} from "./component/index"
 import {Login, Signup, AllProducts, WishList, Home, MyCart, Logout} from "./pages/index"
-
 import MockAPI from "./component/Mockman/Mockman"
- 
- 
+import {useAuthContext} from "./Context/index"
+
+import {ToastContainer} from "react-toastify"
 
 function App() {
+
+  const {token} = useAuthContext()
+
   return (
     <> 
+    <ToastContainer autoClose={1000} />
     <div className="App-container">
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />}></Route> 
-        <Route path="/cart" element={<MyCart/>}></Route> 
-        <Route path="/wishlist" element={<WishList/>}></Route> 
+        <Route path="/cart" element={token? <MyCart/>: <Login />}></Route> 
+        <Route path="/wishlist" element={token? <WishList/> : <Login />}></Route> 
         <Route path="/allproducts" element={<AllProducts/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="/logout" element={<Logout/>}></Route>
+        <Route path="/logout" element={token ? <Logout />: <Login /> }></Route>
         <Route path="/mockman" element={<MockAPI />}> </Route>
       </Routes>
 
