dbcd75760d4bccb94af082debaab72ec76b5bc1b
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-06-05 09:40:23 +0530

    Create razorPayPopup.js

---
+++ src/utils/razorPayPopup.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+
+ 
+  function loadScript(src) {
+    return new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.src = src;
+        script.onload = () => {
+            resolve(true);
+        };
+        script.onerror = () => {
+            resolve(false);
+        };
+        document.body.appendChild(script);
+    });
+}
+
+export async function displayRazorpay() {
+    const res = await loadScript(
+        "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+        alert("Razorpay SDK failed to load. Are you online?");
+        return;
+    }
+
+    // creating a new order
+    const result = await axios.post("http://localhost:5000/payment/orders");
+
+    if (!result) {
+        alert("Server error. Are you online?");
+        return;
+    }
+
+    // Getting the order details back
+    const { amount, id: order_id, currency } = result.data;
+
+    const options = {
+        key: "rzp_test_AlMSQbJbpdb1Nk", // Enter the Key ID generated from the Dashboard
+        amount: amount.toString(),
+        currency: currency,
+        name: "Pearl.",
+        description: "Test Transaction",
+        image: { logo },
+        order_id: order_id,
+        handler: async function (response) {
+            const data = {
+                orderCreationId: order_id,
+                razorpayPaymentId: response.razorpay_payment_id,
+                razorpayOrderId: response.razorpay_order_id,
+                razorpaySignature: response.razorpay_signature,
+            };
+
+            const result = await axios.post("http://localhost:5000/payment/success", data);
+
+            alert(result.data.msg);
+        },
+        prefill: {
+            name: "Pearl",
+            email: "support@peral.com",
+            contact: "+91 1234567",
+        },
+        notes: {
+            address: "India",
+        },
+        theme: {
+            color: "#61dafb",
+        },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+}
